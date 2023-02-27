0e6b3f8287281d69b0abf0f5fe66534003881523
Hanna Kostiana <anna.kostianaia@gmail.com>
2023-02-27 19:49:27 +0200

    styled

--- src/components/ContactElement/ContactElement.js
+++ src/components/ContactElement/ContactElement.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import style from '../ContactElement/ContactElement.module.css';
 import PropTypes from 'prop-types';
+import { FcPhone } from 'react-icons/fc';
+import { IconContext } from "react-icons";
 
 const ContactElement = ({ onDeleteContact, contact: { id, name, number } }) => (
     <li className={style.contact__item}> 
+        <IconContext.Provider value={{ size: "2em" }}>
+            <div>
+                <FcPhone className={style.swing} />
+            </div>
+        </IconContext.Provider>
         <p className={style.contact__text}>{name}</p>
         <p className={style.contact__tel}>{number}</p>
         <button
